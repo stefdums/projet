fbbe5cb8edacc3637aec8790c40174b7c452db0c
stefdums <stephane-dumont@hotmail.fr>
2020-07-20 16:40:10 +0200

    av travail upload message

--- frontend/src/views/Poster.vue
+++ frontend/src/views/Poster.vue
@@ -10,12 +10,32 @@
                 <b-form-input type="text" v-model="titreImage" name="titre" id="titre" required="required"></b-form-input>
             </b-form-group>
 
+            <b-form-group label="type d'envoi">
+                <b-form-radio v-model="selected" id="url" name="image-upload" value="url">Lien de l'image</b-form-radio>
+                <b-form-radio v-model="selected" id="upload" name="image-upload" value="upload">image à upload</b-form-radio>
+            </b-form-group>
+
             <b-form-group class="form-group"
                 id="input-group-url"
                 label="lien de l'image: "
                 label-for="imageurl"
+                v-show="selected === 'url'"               
             >
                 <b-form-input type="url" v-model="imageUrl" name="imageurl" id="image" ></b-form-input>
+            </b-form-group>
+
+            <b-form-group class="form-group"
+            v-show="selected === 'upload'"
+            id="input-group-file"
+            label="image à upload"
+            label-for="imageUrl"
+            
+            >
+
+                <b-form-file type="file" placeholder = "placez votre image " ref='file'
+                v-model='imageUrl'
+                @change="handleFileUpload"></b-form-file>
+            
             </b-form-group>
 
             <b-button id="bouton-valid" @click.prevent="postMessage()" value ="validation" class="btn btn-light bg-light">
@@ -28,7 +48,11 @@
 import axios from 'axios'
 
 export default {
-
+    data (){
+        return {
+            selected: null,
+        }
+    },
     methods: {
         postMessage (){
             console.log('test')
